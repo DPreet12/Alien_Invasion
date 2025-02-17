5a81e4a43084bf67ccf4ce1ab63e2fe3df76da48
DPreet12 <dharampreetsinghsandhu236@gmail.com>
2025-02-16 21:35:34 -0500

    Initial commit after reinitialize

---
+++ character.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Character:
+    """A class a to manage the character"""
+
+    def __init__(self, ai_character):
+        """Initialize the game characterand set its starting position"""
+
+        self.screen = ai_character.screen
+        self.screen_rect = ai_character.screen.get_rect()
+
+        self.image = pygame.image.load("images/red_alien.png")
+        
+        self.image.set_colorkey((255, 255, 255))
+
+        self.image_rect = self.image.get_rect()
+        # put that image in centre of the screen
+        self.image_rect.center = self.screen_rect.center
+        
+
+    def blit_char(self):
+        """Draw the character at its current location"""
+        self.screen.blit(self.image, self.image_rect)
+
+
+
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Intiallize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 100, 255)
+        self.ship_speed = 1.5
